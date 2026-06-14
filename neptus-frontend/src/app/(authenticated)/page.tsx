ac1a51f056c6ce41e3fc35bb10103d06059a5555
David Victor Ferreira Soares <davidvictorjgs@gmail.com>
2026-06-14 00:08:07 -0300

    Escondendo o botão de Calibração

--- neptus-frontend/src/app/(authenticated)/page.tsx
+++ neptus-frontend/src/app/(authenticated)/page.tsx
@@ -232,16 +232,17 @@ export default function Home() {
           />
         </div>
 
-
-        <div className="flex justify-center ">
-            <AppButton className="w-53"
-              variant="secondary"
-              size="lg"
-              onClick={() => router.push("/calibracao")}
-          >
-            Modo calibração
-          </AppButton>
-        </div>
+        { isConnected ? (
+          <div className="flex justify-center ">
+              <AppButton className="w-53"
+                variant="secondary"
+                size="lg"
+                onClick={() => router.push("/calibracao")}
+            >
+              Modo calibração
+            </AppButton>
+          </div>
+        ) : null }  
 
 
       </main>
